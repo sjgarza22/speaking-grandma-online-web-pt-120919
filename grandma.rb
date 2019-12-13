0b28f586693395697055e22a742bcd2faff1caa5
Sara Garza <sjgarza22@gmail.com>
2019-12-13 03:01:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    puts "
-  else
     puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
